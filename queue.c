853940e9954925e3be3eef2e8d6195574f1b4907
victor souza <victor_souzas@yahoo.com.br>
2018-10-26 22:34:44 -0300

    first step accomplished it fails

--- queue.c
+++ queue.c
@@ -1,51 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct node
+struct Node
 {
-	void *value;
+	int value;
 	void *next;
 };
 
-struct queue
+struct Queue
 {
 	void *start;
 	void *end;
+	int size;
 };
 
-void enqueue(void *queue, void *node);
+void enqueue(void *queue, int value);
 void * dequeue(void *queue);
 int is_empty(void *queue);
 
 int main(int argc, char **argv)
 {
-	printf("%s\n", argv[1]);
-
-	FILE *input = fopen(argv[1], "r");
-
-
+	struct Queue *queue = (struct Queue*) malloc(sizeof(struct Queue));
+	enqueue(&queue, 10);
+	enqueue(&queue, 10);
+	enqueue(&queue, 10);
 	/* code */
 	return 0;
 }
 
-void enqueue(void *queue, void *value)
+void enqueue(void *queue, int value)
 {
-	struct queue *new_queue = queue;
-	struct node *new_node;
-	new_node->value = (void *)value;
+	struct Queue *new_queue = (struct Queue*) queue;
+	struct Node *new_node = (struct Node*) malloc(sizeof(struct Node));
+	new_node->value = value;
 	if (is_empty(queue)) {
 		new_queue->start = (void *) new_node;
 		new_queue->end = (void *) new_node;
+		new_queue->size = new_queue->size + 1;
 		return;
 	}
-	struct node *temp = new_queue->end;
+	struct Node *temp = (struct Node*) new_queue->end;
 	temp->next = (void *) new_node;
 	new_queue->end = (void *) new_node;
 }
 
 void * dequeue(void *queue)
 {
-	struct queue *new_queue = queue;
-	struct node *temp = new_queue->start;
+	struct Queue *new_queue = (struct Queue*) queue;
+	struct Node *temp = (struct Node*) new_queue->start;
 	new_queue->start = temp->next;
+	new_queue->size = new_queue->size - 1;
 	return temp;
+}
+
+int is_empty(void *queue)
+{
+	struct Queue *new_queue = (struct Queue*) queue;
+	return new_queue->size == 0;
 }
